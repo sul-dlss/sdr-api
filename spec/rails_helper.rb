5f38092fa3242cf011e89da18c66d66003d3b1dc
Naomi Dushay <ndushay@stanford.edu>
2020-01-07 18:27:12 -0800

    rubocop: fix IfUnlessModifier

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-if Rails.env.production?
-  abort('The Rails environment is running in production mode!')
-end
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
 require 'webmock/rspec'
 
