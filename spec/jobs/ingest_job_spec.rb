1b0e3a07d49b8bf7c420571441c5f7939d09a512
Justin Littman <justinlittman@stanford.edu>
2020-01-21 11:46:52 -0500

    Sets lane_id to low when creating workflow.
    closes #92

--- spec/jobs/ingest_job_spec.rb
+++ spec/jobs/ingest_job_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe IngestJob, type: :job do
     run
     expect(File.read("#{assembly_dir}/content/file2.txt")).to eq 'HELLO'
     expect(File).to exist("#{assembly_dir}/metadata/contentMetadata.xml")
-    expect(client).to have_received(:create_workflow_by_name).with(druid, 'assemblyWF', version: 1)
+    expect(client).to have_received(:create_workflow_by_name).with(druid, 'assemblyWF', version: 1, lane_id: 'low')
     expect(result).to be_complete
   end
 end
