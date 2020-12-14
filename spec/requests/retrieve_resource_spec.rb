668ca24b2149ab1ef3b99bfb7d34d3f5a82b80c5
Justin Coyne <jcoyne@justincoyne.com>
2020-12-14 10:51:33 -0600

    Add an endpoint to expose the metadata

---
+++ spec/requests/retrieve_resource_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Retrieve a resource' do
+  let(:request) do
+    <<~JSON
+      {
+        "label":"hello",
+        "externalIdentifier":"druid:bc999dg9999",
+        "version":2,
+        "type":"#{Cocina::Models::Vocab.book}",
+        "access": {
+          "access":"world",
+          "copyright":"All rights reserved unless otherwise indicated.",
+          "download":"none",
+          "useAndReproductionStatement":"Property rights reside with the repository...",
+          "embargo": {
+            "releaseDate": "2029-06-22T07:00:00.000+00:00",
+            "access": "world",
+            "useAndReproductionStatement": "Whatever you want"
+          }
+        },
+        "administrative": {
+          "hasAdminPolicy":"druid:bc123df4567",
+          "partOfProject":"Google Books",
+          "releaseTags":[]
+        },
+        "identification": {
+          "catalogLinks": [
+              {
+                "catalog":"symphony",
+                "catalogRecordId":"123456"
+              }
+          ],
+          "sourceId":"googlebooks:stanford_82323429"
+        },
+        #{structural}
+      }
+    JSON
+  end
+
+  let(:structural) do
+    <<~JSON
+      "structural":{
+        "isMemberOf":["druid:fg123hj4567"],
+        "contains":[
+          {
+            "type":"http://cocina.sul.stanford.edu/models/fileset.jsonld",
+            "externalIdentifier":"9999",
+            "label":"Page 1",
+            "structural":{
+              "contains":[]
+            },
+            "version":2
+          }
+        ]
+      }
+    JSON
+  end
+
+  before do
+    stub_request(:get, 'http://localhost:3003/v1/objects/druid:bc999dg9999')
+      .to_return(status: 200, body: request, headers: {})
+  end
+
+  it 'returns the cocina model' do
+    get '/v1/resources/druid:bc999dg9999',
+        headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{jwt}" }
+    expect(response).to be_successful
+    expect(JSON.parse(response.body)['externalIdentifier']).to eq 'druid:bc999dg9999'
+  end
+end
