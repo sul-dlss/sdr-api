fb4f2bccb44a3db4c027b43692c71a8f753860af
Peter Mangiafico <peter@mangiafico.org>
2023-08-18 13:49:28 -0700

    honeybadger check-in for cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,8 +10,14 @@
 # set :output, "/path/to/my/cron_log.log"
 #
 # Learn more: https://github.com/javan/whenever
+require_relative 'environment'
+
+job_type :runner_hb,
+         "cd :path && bin/rails runner -e :environment ':task' " \
+         "&& curl 'https://api.honeybadger.io/v1/check_in/:check_in' :output"
 
 every :day do
   set :output, standard: 'log/uploads_sweeper.log', error: 'log/uploads_sweeper.error.log'
-  runner 'DirectUploadsSweeper.new(strategy: SelectOutdatedUploadsStrategy).sweep'
+  set :check_in, Settings.honeybadger_checkins.direct_uploads_sweeper
+  runner_hb 'DirectUploadsSweeper.new(strategy: SelectOutdatedUploadsStrategy).sweep'
 end
