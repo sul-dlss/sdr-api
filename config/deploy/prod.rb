be98b7f78582ad8eef24b5dfbd45baf7c2797d96
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-05-09 13:45:37 -0700

    Remove explicit one-time key setup

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
 server 'sdr-api-prod.stanford.edu', user: 'sdr', roles: %w[app db web]
-
-Capistrano::OneTimeKey.generate_one_time_key!
--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
 server 'sdr-api-qa.stanford.edu', user: 'sdr', roles: %w[app db web]
-
-Capistrano::OneTimeKey.generate_one_time_key!
