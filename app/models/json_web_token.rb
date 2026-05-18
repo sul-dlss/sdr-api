9be92efdd34d0574d14027443c37a1f012889dfb
Laura Wrubel <lwrubel@stanford.edu>
2026-05-18 11:37:56 -0400

    specify hmac algorithm

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -3,11 +3,11 @@
 class JsonWebToken
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, Settings.sdr_api.hmac_secret)
+    JWT.encode(payload, Settings.sdr_api.hmac_secret, 'HS256')
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, Settings.sdr_api.hmac_secret)[0]
+    decoded = JWT.decode(token, Settings.sdr_api.hmac_secret, true, { algorithm: 'HS256' })[0]
     ActiveSupport::HashWithIndifferentAccess.new decoded
   end
 end
